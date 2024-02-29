6f977d72005b845e25809ea54047a4701fa0cedf
Fernando <ferelbue@gmail.com>
2024-02-29 09:30:44 +0100

    feat:refactor database folder

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 
 import "dotenv/config"; //archivo con las variables de entorno
 import { app } from "./app";
-import { AppDataSource } from "./db";
+import { AppDataSource } from "./database/db";
 
 //Si existe un puerto en el archivo .env lo usa. Si no usa el 4001
 // El puerto 4000 es el puerto de mi ordenador y el 3307/8/9 es el de la base de datos
